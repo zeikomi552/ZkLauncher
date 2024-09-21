0f61ada4198d7e476dc97c4630132a04b7e652dd
zeikomi552 <premium.tsubu@gmail.com>
2024-09-21 18:54:45 +0900

    ウィンドウの呼び出し処理を追加

--- ZkLauncher/ViewModels/UserControl/ucControlPanelViewModel.cs
+++ ZkLauncher/ViewModels/UserControl/ucControlPanelViewModel.cs
@@ -34,10 +34,39 @@ namespace ZkLauncher.ViewModels.UserControl
         }
         #endregion
 
-        public ucControlPanelViewModel(IDisplayEmentsCollection displayElements)
+        private IDialogService? _dialogService;
+        public ucControlPanelViewModel(IDialogService dialogService, IDisplayEmentsCollection displayElements)
         {
+            _dialogService = dialogService;
             this.DisplayElements = displayElements;
 
+        }
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private DelegateCommand _showDialogCommand;
+        public DelegateCommand ShowDialogCommand =>
+            _showDialogCommand ?? (_showDialogCommand = new DelegateCommand(ShowDialog));
+
+        private void ShowDialog()
+        {
+            var message = "This is a message that should be shown in the dialog.";
+            //using the dialog service as-is
+            _dialogService.ShowDialog("ucViewerPanel", new DialogParameters($"message={message}"), r =>
+            {
+                if (r.Result == ButtonResult.None)
+                    Title = "Result is None";
+                else if (r.Result == ButtonResult.OK)
+                    Title = "Result is OK";
+                else if (r.Result == ButtonResult.Cancel)
+                    Title = "Result is Cancel";
+                else
+                    Title = "I Don't know what you did!?";
+            });
         }
     }
 }
--- ZkLauncher/ViewModels/UserControl/ucViewerPanelViewModel.cs
+++ ZkLauncher/ViewModels/UserControl/ucViewerPanelViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZkLauncher.Models;
 
 namespace ZkLauncher.ViewModels.UserControl
 {
@@ -58,6 +59,36 @@ namespace ZkLauncher.ViewModels.UserControl
         public virtual void OnDialogOpened(IDialogParameters parameters)
         {
             Message = parameters.GetValue<string>("message");
+        }
+
+        #region 表示要素
+        /// <summary>
+        /// 表示要素
+        /// </summary>
+        IDisplayEmentsCollection? _DisplayElements;
+        /// <summary>
+        /// 表示要素
+        /// </summary>
+        public IDisplayEmentsCollection? DisplayElements
+        {
+            get
+            {
+                return _DisplayElements;
+            }
+            set
+            {
+                if (_DisplayElements == null || !_DisplayElements.Equals(value))
+                {
+                    _DisplayElements = value;
+                    RaisePropertyChanged("DisplayElements");
+                }
+            }
+        }
+        #endregion
+
+        public ucViewerPanelViewModel(IDisplayEmentsCollection displayElements)
+        {
+            this.DisplayElements = displayElements;
         }
     }
 }
