c4f39ff91e09b37b586efee02fee15301e5fac53
zeikomi552 <premium.tsubu@gmail.com>
2024-10-19 18:29:50 +0900

    画面キャプチャの保存処理を作成

--- ZkLauncher/ViewModels/UserControl/ucViewerPanelViewModel.cs
+++ ZkLauncher/ViewModels/UserControl/ucViewerPanelViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Threading;
 using ZkLauncher.Common.Utilities;
 using ZkLauncher.Models;
@@ -66,6 +68,23 @@ namespace ZkLauncher.ViewModels.UserControl
             get { return _message; }
             set { SetProperty(ref _message, value); }
         }
+
+        #region 背景の保存先ディレクトリ
+        /// <summary>
+        /// 背景の保存先ディレクトリ
+        /// </summary>
+        private string ImageSaveDirectory
+        {
+            get
+            {
+                // アプリケーションフォルダの取得
+                var dir = Path.Combine(PathManager.GetApplicationFolder(), "SaveImage", DateTime.Today.ToString("yyyyMM"));
+                PathManager.CreateDirectory(dir);
+
+                return dir;
+            }
+        }
+        #endregion
 
         #region 表示要素
         /// <summary>
@@ -309,6 +328,10 @@ namespace ZkLauncher.ViewModels.UserControl
         }
         #endregion
 
+        #region 画面のリロード処理
+        /// <summary>
+        /// 画面のリロード処理
+        /// </summary>
         public void Reload()
         {
             try
@@ -320,5 +343,65 @@ namespace ZkLauncher.ViewModels.UserControl
                 ShowMessage.ShowErrorOK(e.Message, "Error");
             }
         }
+        #endregion
+
+        #region ページの保存処理
+        /// <summary>
+        /// ページの保存処理
+        /// </summary>
+        public void SavePage()
+        {
+            try
+            {
+                var ctrl = this.DisplayElements!.SelectedItem.WebView2Object!;
+                var targetPoint = ctrl.PointToScreen(new System.Windows.Point(0.0d, 0.0d));
+
+                // キャプチャ領域の生成
+                var targetRect = new Rect(targetPoint.X, targetPoint.Y, ctrl.ActualWidth, ctrl.ActualHeight);
+
+                //// スクリーンショット実行
+                ExcuteScreenShot(targetRect, fileName(ImageSaveDirectory));
+            }
+            catch (Exception e)
+            {
+                ShowMessage.ShowErrorOK(e.Message, "Error");
+            }
+        }
+        #endregion
+
+        #region ファイルパスの作成処理
+        /// <summary>
+        /// ファイルパスの作成処理
+        /// </summary>
+        /// <param name="folderPath">フォルダパス</param>
+        /// <returns>ファイルパス</returns>
+        private string fileName(string folderPath)
+        {
+            var dtNow = DateTime.Now;
+            var file = dtNow.ToString("yyyyMMdd_hhmmss") + dtNow.Millisecond.ToString() + ".jpg";
+            return Path.Combine(folderPath, file);
+        }
+        #endregion
+
+        #region スクリーンショットの作成処理
+        /// <summary>
+        /// スクリーンショットの作成処理
+        /// </summary>
+        /// <param name="rect">矩形</param>
+        /// <param name="fileName">ファイル名</param>
+        private void ExcuteScreenShot(Rect rect, string fileName)
+        {
+            // 矩形と同じサイズのBitmapを作成
+            using (var bitmap = new Bitmap((int)rect.Width, (int)rect.Height))
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                // 画面から指定された矩形と同じ条件でコピー
+                graphics.CopyFromScreen((int)rect.X, (int)rect.Y, 0, 0, bitmap.Size);
+
+                // 画像ファイルとして保存
+                bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+        #endregion
     }
 }
